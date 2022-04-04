65f6bb14770721b36e65b1025640ef79551b344f
dungxibo123 <votiendung0805@gmail.com>
2022-04-04 10:58:28 +0700

    add generate_run_file.py

--- src/generate_run_file.py
+++ src/generate_run_file.py
@@ -1,19 +1,21 @@
 
 
-datasets = ["Cora", "Citeseer", "Pubmed", "CoauthorCS"]
-depths = [1,2,3,4,14,28,56]
-step_sizes = [0.00001, 0.00005, 0.0001, 0.0005, 0.001, 0.005]
+datasets = ["Cora", "Citeseer"]
+depths = [3,4]
+step_sizes = [0.000001, 0.000005, 0.00001, 0.00005]
 norm_types = ["norm", "frobenius_norm"]
-alphas = [0.5, 1.0, 1.5, 2.0, 2.5]
+alphas = [4.0, 3.5]
+truncate_coeffs = [2.0, 2.5]
+
 
 
 
 for dataset in datasets:
-	f = open(f"run_{dataset}.sh", "w")
-	for depth in depths:
-		for step_size in step_sizes:
-			for norm_type in norm_types:
-				for alpha in alphas:
-					f.write(f"python3 run_grand_ex.py --dataset {dataset} --depth {depth} --step_size {step_size} --discritize_type {norm_type} --norm_exp {alpha} --run_time A10_FPT_Cloud --truncate_norm\n")
-	f.close()
-
+  f = open(f"run_{dataset}.sh", "w")
+  for depth in depths:
+    for step_size in step_sizes:
+      for norm_type in norm_types:
+        for alpha in alphas:
+          for t_co in truncate_coeffs:
+            f.write(f"python3 run_grand_ex.py --dataset {dataset} --depth {depth} --step_size {step_size} --discritize_type {norm_type} --norm_exp {alpha} --run_time A10_FPT_Cloud_second --truncate_coeff {t_co} --truncate_norm\n")
+  f.close()
